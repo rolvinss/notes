33780f740c12f2db701a245d2853a3de4c1ba16c
Jaswant Singh <rolvinss@gmail.com>
2024-07-23 19:19:29 +0530

    Update FeatureTile.js

--- FeatureTile.js
+++ FeatureTile.js
@@ -1,1 +1,31 @@
-    const updatedProps = { ...appointmentProps, cartDetails: { ...appointmentProps.cartDetails, shippingDetails: { deliveryDate: formattedDatetype1 } } };
+export const dummyFormattedDates = () => {
+  const today = new Date();
+
+  // Subtract 7 days from today's date
+  const sevenDaysAgo = new Date(today);
+  sevenDaysAgo.setDate(today.getDate() - 7);
+
+  // Format the date to "MM/DD/YYYY"
+  const formattedDatetype1 =
+    (sevenDaysAgo.getMonth() + 1).toString().padStart(2, '0') +
+    '/' +
+    sevenDaysAgo.getDate().toString().padStart(2, '0') +
+    '/' +
+    sevenDaysAgo.getFullYear();
+
+  // Format the date to format "Tuesday, July 15, 2024"
+  const formattedDateType2 =
+    helpers.DAYS[sevenDaysAgo.getDay()] +
+    ', ' +
+    helpers.MONTHS[sevenDaysAgo.getMonth()] +
+    ' ' +
+    sevenDaysAgo.getDate() +
+    ', ' +
+    sevenDaysAgo.getFullYear();
+  const formattedDateType3 = helpers.MONTHS[sevenDaysAgo.getMonth()] + ' ' + sevenDaysAgo.getFullYear();
+  return {
+    formattedDatetype1,
+    formattedDateType2,
+    formattedDateType3,
+  };
+};
